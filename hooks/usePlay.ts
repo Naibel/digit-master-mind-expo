03c80b4b30e1675724bfd53e300876ed01b30e5d
Naibel <dorian.belhaj@gmail.com>
2025-03-13 17:52:27 +0100

    ✏️ (usePlay) fix "chosen" typo

--- hooks/usePlay.ts
+++ hooks/usePlay.ts
@@ -1,13 +1,13 @@
 const usePlay = () => {
-  const play = (attempt: string, choosen: string) => {
+  const play = (attempt: string, chosenNumber: string) => {
     let bulls = 0;
     let cows = 0;
 
-    for (let i = 0; i < choosen.toString().length; i++) {
-      if (attempt.toString().charAt(i) === choosen.toString().charAt(i)) {
+    for (let i = 0; i < chosenNumber.toString().length; i++) {
+      if (attempt.toString().charAt(i) === chosenNumber.toString().charAt(i)) {
         cows++;
       } else if (
-        attempt.toString().indexOf(choosen.toString().charAt(i)) > -1
+        attempt.toString().indexOf(chosenNumber.toString().charAt(i)) > -1
       ) {
         bulls++;
       }
